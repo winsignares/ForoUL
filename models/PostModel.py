19f2e174e8d077e6d13d9c5f1caec6dcd7843947
william <elhacker01@nose.com>
2024-04-04 11:14:31 -0500

    mejora de model post

--- models/PostModel.py
+++ models/PostModel.py
@@ -6,10 +6,11 @@ class Post (bd.Model):
     
     idForo = bd.Column(bd.Integer, primary_key = True)
     Comentario = bd.Column(bd.String(200))
-    id_User = bd.Column(bd.Integer, foreing_key = True)
+    id_User = bd.Column(bd.Integer, bd.Foreignkey('tblUsers.id'))
 
-    def __init__(self, Comentario):
+    def __init__(self, Comentario,id_User):
         self.Comentario = Comentario
+        self.id_User = id_User
 
 with app.app_context():
     bd.create_all()
