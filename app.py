889b2bafc34be4ed96e09ed5950d70c390979260
Keiler <Deattleking@yo.com>
2024-04-11 10:18:12 -0500

    controller

--- app.py
+++ app.py
@@ -1,22 +1,15 @@
 from flask import Flask, render_template, request, json, jsonify
 from config.db import app
 
-from models.UserModel import Users, UsersSchema
-from models.PostModel import Post, PostsSchema
-from models.CommentsModel import Comment, CommentsSchema 
+from controllers.UserController import ruta_user
+from controllers.CommentController import ruta_comment
 
-user_schema = UsersSchema()
-users_schema = UsersSchema(many=True)
-
-post_schema = PostsSchema()
-posts_schema = PostsSchema(many=True)
-
-comment_schema = CommentsSchema()
-comments_schema = CommentsSchema(many=True)
+app.register_blueprint(ruta_user, url_prefix="/controller")
+app.register_blueprint(ruta_comment, url_prefix="/controller")
 
 @app.route('/', methods=['GET'])
 def index():
-    return "Hola Mundo"
+    return "Hola Mundo index"
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0')
--- controllers/UserController.py
+++ controllers/UserController.py
@@ -1,1 +1,13 @@
-print("hola")
+from flask import Blueprint, Flask, render_template, request
+from config.db import bd, ma, app
+
+from models.UserModel import Users, UsersSchema
+
+ruta_user = Blueprint("clave", __name__)
+
+user_schema = UsersSchema()
+users_schema = UsersSchema(many=True)
+
+@ruta_user.route('/User')
+def indexuser():
+    return 'Hola Mundo controlador User'
