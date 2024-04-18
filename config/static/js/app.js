aed2d0f8f300d537656b8e2a6b80134b2cdb53b3
william <elhacker01@nose.com>
2024-04-18 09:47:04 -0500

    agregar data

--- config/static/js/app.js
+++ config/static/js/app.js
@@ -3,3 +3,23 @@ function saludar() {
     let email = document.getElementById("exampleInputEmail1")
     alert("Hola Mundo "+ email.value);
 }
+/***
+ * <tr>
+        <th scope="row">1</th>
+        <td>Mark</td>
+        <td>Otto</td>
+        <td>@mdo</td>
+      </tr>
+ */
+function SaveUser() {
+    let email = document.getElementById("exampleInputEmail1")
+    let fullname = document.getElementById("FullName")
+    let tcuerpo = document.getElementById("tcuerpo")
+    let data ='';
+    data = '<tr>';
+    data += '<th scope="row">1</th>';
+    data += '<td>'+email.value+'</td>';
+    data += '<td>'+fullname.value+'</td>';
+    data += '</tr>';
+    tcuerpo.innerHTML = data;
+}
