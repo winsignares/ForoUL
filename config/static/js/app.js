ce22c13f7af17b32f939e6b87c0c1159deda08bf
Rafaarfa10 <rafael-arizaf@unilibre.edu.co>
2024-04-18 10:35:21 -0500

    update

--- config/static/js/app.js
+++ config/static/js/app.js
@@ -11,16 +11,28 @@ function saludar() {
         <td>@mdo</td>
       </tr>
  */
-function SaveUser() {
-    let email = document.getElementById("exampleInputEmail1")
-    let fullname = document.getElementById("FullName")
-    let tcuerpo = document.getElementById("tcuerpo")
-    let data ='';
-    data = '<tr>';
-    data += '<th scope="row">1</th>';
-    data += '<td>'+email.value+'</td>';
-    data += '<td>'+fullname.value+'</td>';
-    data += '</tr>';
-    tcuerpo.appendChild = data;
-    swal("Datos Guardado!", "Puede continuar!", "success");
-}
+      function SaveUser() {
+       
+        let emailInput = document.getElementById("exampleInputEmail1");
+        let fullnameInput = document.getElementById("FullName");
+        let tbody = document.getElementById("tcuerpo");
+    
+       
+        let email = emailInput.value;
+        let fullname = fullnameInput.value;
+    
+        
+        let newRow = document.createElement("tr");
+        newRow.innerHTML = `
+            <th scope="row">${tbody.children.length + 1}</th>
+            <td>${email}</td>
+            <td>${fullname}</td>
+        `;
+    
+        
+        tbody.appendChild(newRow);
+       
+        emailInput.value = "";
+        fullnameInput.value = "";
+    }
+    
